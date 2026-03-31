a921e31e97d9e2c4b07111fba4201a205aca0333
Parisa Montakhabi <parisa.montakhabi@gmail.com>
2026-03-31 19:13:53 +0200

    chore : cleanUp the structure

--- src/app/api/customers/route.ts
+++
@@ -1,6 +0,0 @@
-import { customerMockData } from '@/mock/customerMockData';
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json(customerMockData);
-}
